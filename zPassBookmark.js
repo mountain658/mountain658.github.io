9674b2ef2034f54bacfe18061a89d8a8bf18a13e
human456 <francisco.gonzalez2025@geringschools.net>
2022-01-10 11:25:37 -0700

    Update zPassBookmark.js

--- zPassBookmark.js
+++ zPassBookmark.js
@@ -1,6 +1,6 @@
 javascript:(function(){javascript:(function(){javascript:var frame = document.createElement('iframe'); 
 frame.id = "Mountain Game";
-frame.src="https://mountain-game.human456.repl.co"; frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%"; frame.style.height="669px"; frame.style.width="812px"; frame.style.zIndex="100000"; document.body.appendChild(frame); var btn= document.createElement("button"); btn.style.position="fixed"; btn.style.top="10%"; btn.style.right="84%"; btn.zIndex="100000"; btn.innerHTML="Close";btn.onclick = function(){boomFrame();};function boomFrame() {var myframe = document.getElementsByTagName("iframe")[0];myframe.parentNode.removeChild(frame);btn.remove();}document.body.appendChild(btn);
+frame.src="https://rebrand.ly/human456"; frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%"; frame.style.height="669px"; frame.style.width="812px"; frame.style.zIndex="100000"; document.body.appendChild(frame); var btn= document.createElement("button"); btn.style.position="fixed"; btn.style.top="10%"; btn.style.right="84%"; btn.zIndex="100000"; btn.innerHTML="Close";btn.onclick = function(){boomFrame();};function boomFrame() {var myframe = document.getElementsByTagName("iframe")[0];myframe.parentNode.removeChild(frame);btn.remove();}document.body.appendChild(btn);
 
 document.onkeydown = function(evt) {
     evt = evt || window.event;

74356a542b1bc8ac6e01cb3d85b99d3c0e3a6804
mountain658 <58858147+mountain658@users.noreply.github.com>
2023-03-31 11:44:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 (function(){
-
+//Super important scripts here
+  //WARNING: DELETING THIS FILE FROM IT'S CDN COULD CAUSE DISASTERS IN THE FUTURE
+  //Just don't delete it, it does no harm an only helps the user
+  //I will put updates here for out-of-date sites
+  
+  //DO NOT DELTE
 })();

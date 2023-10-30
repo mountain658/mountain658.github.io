5e4982c676ab1ea56cc5d906c73ff076635be1f9
mountain658 <58858147+mountain658@users.noreply.github.com>
2023-10-30 11:11:25 -0600

    Add files via upload

---
+++ g/mario/README.md
@@ -0,0 +1,2 @@
+# PlayMario.org
+PlayMario.org is a free HTML5 remake of original Super Mario Bros. Play it here &mdash;  https://playmario.org
---
+++ g/mario/index.js
@@ -0,0 +1,24 @@
+﻿var time = Date.now();
+document.onreadystatechange = function (event) {
+    if (event.target.readyState !== "complete") {
+        return;
+    }
+
+    var UserWrapper = new UserWrappr.UserWrappr(PlayMarioJas.PlayMarioJas.prototype.proliferate(
+        {
+            "GameStartrConstructor": PlayMarioJas.PlayMarioJas
+        }, PlayMarioJas.PlayMarioJas.settings.ui, true));
+
+    console.log("PlayMario took " + (Date.now() - time) + " milliseconds to start.");
+    UserWrapper.GameStarter.UsageHelper.displayHelpMenu();
+};
+  
+var manifest_iframe = document.createElement('iframe');
+		manifest_iframe.setAttribute("width","0")
+		manifest_iframe.setAttribute("height","0")
+		manifest_iframe.setAttribute("scrolling","no")
+		manifest_iframe.setAttribute("frameborder","0")
+		manifest_iframe.setAttribute("src","assets/manifest.html")
+		manifest_iframe.setAttribute("style","visibility:hidden;display:none")
+		manifest_iframe.setAttribute("seamless","seamless")
+document.body.appendChild(manifest_iframe);

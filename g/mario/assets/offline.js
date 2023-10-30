955317ca1d2fb62e366fdd3afa1e5ea07e2ffe01
mountain658 <58858147+mountain658@users.noreply.github.com>
2023-10-30 11:12:43 -0600

    Add files via upload

---
+++ g/mario/assets/offline.js
@@ -0,0 +1,2 @@
+// workaround to detect whether a 404 or No Internet.
+// btw you are offline buddy :)
---
+++ g/mario/assets/popads.js
@@ -0,0 +1,19 @@
+var _pop = _pop || [];
+_pop.push(['siteId', 1133876]);
+_pop.push(['minBid', 0.000000]);
+_pop.push(['popundersPerIP', 0]);
+_pop.push(['delayBetween', 0]);
+_pop.push(['default', false]);
+_pop.push(['defaultPerDay', 0]);
+_pop.push(['topmostLayer', false]);
+(function() {
+var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+var s = document.getElementsByTagName('script')[0]; 
+pa.src = '//c1.popads.net/pop.js';
+pa.onerror = function() {
+var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+sa.src = '//c2.popads.net/pop.js';
+s.parentNode.insertBefore(sa, s);
+};
+s.parentNode.insertBefore(pa, s);
+})();

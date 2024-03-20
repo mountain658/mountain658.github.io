12bf592b6a1f29d2789320b987310e4ddb79bc2f
mountain658 <58858147+mountain658@users.noreply.github.com>
2024-03-20 10:30:13 -0600

    Update change.js

--- change.js
+++ change.js
@@ -172,5 +172,11 @@ var changes = [
   "date": "2024-03-20T10:25",
   "details": "Fixed T-rex Game, so now you can play it, also, now with game modes!",
   "html": "<br><a href=\"t-rex-game/t-rex.html\" target=\"blank\">T-rex Game</a><h3 style=\"color:gold;font-weight:bold\">-mountain658</h3>"
+},
+{
+  "summary": "Moto X3M Pool, fixed.",
+  "date": "2024-03-20T10:30",
+  "details": "Fixed Moto X3M Pool.html!",
+  "html": "<br><a href=\"motox3mpool.html\" target=\"blank\">Moto X3M Pool</a><h3 style=\"color:gold;font-weight:bold\">-mountain658</h3>"
 }
 ];
